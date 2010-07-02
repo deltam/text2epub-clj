8d1b397f90f956c90c91c080669d162fa6aac464
deltam <deltam@gmail.com>
2010-07-02 19:29:32 +0900

    modify comment

--- src/text2epub/core.clj
+++ src/text2epub/core.clj
@@ -138,7 +138,6 @@
         epub_title (last files)
         html_name "section01.xhtml"
         id (str (rand-int 999999999))]
-    (println args)
     (make-meta-inf)
     (out-content-opf epub_title id html_name)
     (out-ncx id [html_name])

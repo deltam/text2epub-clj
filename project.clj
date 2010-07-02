753514f9c1269156df474c388defa29c44ef5bd5
deltam <deltam@gmail.com>
2010-07-02 18:52:49 +0900

    alpha version

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject text2epub "0.0.1-SNAPSHOT"
+  :description "convert plain text into .epub"
+  :url "http://github.com/deltam/text2epub"
+  :main text2epub.core
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [hiccup "0.2.6"]])
+
---
+++ test/text2epub/core_test.clj
@@ -0,0 +1,6 @@
+(ns text2epub.core-test
+  (:use [text2epub.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))

62c56249bf9354fad2954769a0519cf031a35adc
deltam <deltam@gmail.com>
2010-07-06 02:01:09 +0900

    change project name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject text2epub "0.0.1-SNAPSHOT"
+(defproject text2epub-clj "0.0.1-SNAPSHOT"
   :description "convert plain text into .epub"
-  :url "http://github.com/deltam/text2epub"
+  :url "http://github.com/deltam/text2epub-clj"
   :main text2epub.core
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]

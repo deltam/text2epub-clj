8b4f42c69ca32ff61b87bd685de259c823611654
deltam <deltam@gmail.com>
2010-07-08 01:19:06 +0900

    epub.clj: change to make "mimetype" file zipf.clj: devided to-zip, -> add-storefile, add-deflated-file, add-text-file

--- src/text2epub/core.clj
+++ src/text2epub/core.clj
@@ -13,6 +13,7 @@
         epub_title (last files)
         html_name "section01.xhtml"
         id (str (rand-int 999999999))]
+    (mimetype)
     (make-meta-inf)
     (out-content-opf epub_title id html_name)
     (out-ncx id [html_name])
--- src/text2epub/zipf.clj
+++ src/text2epub/zipf.clj
@@ -1,25 +1,77 @@
+; zip file output
+
 (ns text2epub.zipf
   (:use [text2epub epub])
   (:import [java.util.zip ZipEntry ZipOutputStream CRC32]
            [java.io InputStreamReader FileOutputStream FileInputStream]))
 
-      
-(defn to-zip [epub_name]
-  (let [zos (ZipOutputStream. (FileOutputStream. epub_name))
+
+(defn open-zip [f]
+  (ZipOutputStream. (FileOutputStream. f)))
+
+
+(defn add-store-file [#^ZipOutputStream zos f]
+  (. zos setMethod ZipOutputStream/STORED)
+  (let [fis (FileInputStream. f)
+        ze (ZipEntry. f)
+        crc (CRC32.)
+        buf (byte-array 256)
+        count (. fis read buf 0 256)]
+    (. ze setSize count)
+    (. crc update buf)
+    (. ze setCrc (. crc getValue))
+    (. zos putNextEntry ze)
+    (. zos write buf)
+    (. zos closeEntry)))
+
+
+(defn add-deflated-file [#^ZipOutputStream zos f]
+    (. zos setMethod ZipOutputStream/DEFLATED)
+    (. zos putNextEntry (ZipEntry. f))
+    (let [fis (FileInputStream. f)
+          buf (byte-array 1024)]
+      (loop [count (. fis read buf 0 1024)]
+        (if (not (= count -1))
+          (. zos write buf 0 count))
+        (if (= count -1)
+          nil
+          (recur (. fis read buf 0 1024))))
+      (. zos closeEntry)))
+
+
+(defn add-text-file [#^ZipOutputStream zos f]
+  (. zos putNextEntry (ZipEntry. f))
+  (let [fis (InputStreamReader. (FileInputStream. f) "UTF-8")
+        buf (char-array 1024)]
+    (loop [count (.fis read buf 0 1024)]
+      (if (not (= count -1))
+        (let [s (String. buf 0 count)
+              b (. s getBytes "UTF-8")
+              len (alength b)]
+          (. zos write b 0 len)))
+      (if (= count -1)
+        nil
+        (recur (. fis read buf 0 1024))))
+    (. zos closeEntry)))
+
+  
+(defn to-zip [f]
+  (let [zos (ZipOutputStream. (FileOutputStream. f))
         metaf ["META-INF/container.xml" "content.opf" "toc.ncx"]
         secf  ["section01.xhtml"]]
     ; 非圧縮
+;    (add-store-file zos "mimetype")
     (. zos setMethod ZipOutputStream/STORED)
-    (let [data (. mimetype getBytes)
+    (let [data (. "application/epub+zip" getBytes)
           ze (ZipEntry. "mimetype")
           crc (CRC32.)]
-      (. ze setSize (. mimetype length))
+      (. ze setSize (. "application/epub+zip" length))
       (. crc update data)
       (. ze setCrc (. crc getValue))
       (. zos putNextEntry ze)
       (. zos write data)
       (. zos closeEntry))
-    ; 圧縮
+   ; 圧縮
     (. zos setMethod ZipOutputStream/DEFLATED)
     ; metadata
     (doseq [f metaf]

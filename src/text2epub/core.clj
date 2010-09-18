7b12fb03c5dcc0a0dda72d8257ce9f4c216f4082
fukamachi <e.arrows@gmail.com>
2010-09-19 00:54:10 +0900

    refactored main

--- src/text2epub/core.clj
+++ src/text2epub/core.clj
@@ -2,13 +2,10 @@
   (:gen-class)
   (:use [clj-epub epub]))
 
-
 ; main
 ; usage: CMD output.epub epub-title <textfile>..
 (defn -main [& args]
-  (let [epub-name  (first args)
-        epub-title (second args)
-        texts      (drop 2 args)]
-    (if (= 0 (count args))
+  (let [[epub-name epub-title & texts] args]
+    (if (nil? epub-name)
       (println "Usage: java -jar text2epub-clj-*-standalone.jar output.epub \"epub title\" <textfiles>..")
       (gen-epub epub-name epub-title texts))))

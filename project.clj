3314936e1ef6b9f1a38855ec5da4cf289927f87f
fukamachi <e.arrows@gmail.com>
2010-09-19 02:02:08 +0900

    added lein-run and `lein run convert'

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "convert plain text into .epub"
   :url "http://github.com/deltam/text2epub-clj"
   :main text2epub.core
+  :run-aliases {:convert [text2epub.core -main]}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [hiccup "0.2.6"]])
-
+                 [hiccup "0.2.6"]]
+  :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]])

81f6db2c1ad3d9f9ad120fa08191c5eb3e57f6be
deltam <deltam@gmail.com>
2010-10-24 13:32:27 +0900

    move file

--- test/text2epub/test/epub.clj
+++
@@ -1,7 +0,0 @@
-(ns text2epub.test.epub
-  (:use [text2epub.epub] :reload-all)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false))
-
--- test/text2epub/test/zipf.clj
+++
@@ -1,7 +0,0 @@
-(ns text2epub.test.zipf
-  (:use [text2epub.zipf] :reload-all)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false))
-

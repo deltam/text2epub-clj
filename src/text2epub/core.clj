74ff33cd42f62847bdb3f145ce141483f9933c2d
deltam <deltam@gmail.com>
2011-01-15 17:00:43 +0900

    Added option "standby-mode" [-standby -sb], output EPUB repeatedly when modify input files.

--- src/text2epub/core.clj
+++ src/text2epub/core.clj
@@ -8,29 +8,53 @@
            [java.io File]))
 
 
-; main
+(defn- convert [output epub-info]
+  (let [epub (text->epub epub-info)]
+    (epub->file epub output)
+    (println "INPUT:  " (:input epub-info))
+    (println "TITLE:  " (:title epub-info))
+    (println "OUTPUT: " output)))
+
+(defn- file-status [filenames]
+  (reduce into (map #(hash-map % (.lastModified (File. %))) filenames)))
+
+(defn- dateformat [datetime]
+  (let [form (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss")]
+    (.format form datetime)))
+
+(defn- standby-loop [output epub-info]
+  (let [inputs (:input epub-info)
+        last-status (atom (file-status inputs))]
+    (println "standby-mode: exit on Ctrl-c")
+    (convert output epub-info)
+    (loop [now-status (file-status inputs)]
+      (if (some #(> (now-status %) (@last-status %)) inputs)
+        (do (println "Update EPUB: " (dateformat (java.util.Date.)))
+            (convert output epub-info)
+            (reset! last-status now-status)))
+      (Thread/sleep 1000) ; interval
+      (recur (file-status inputs)))))
+
+
 (defn -main
   ([] (-main "--help"))
   ([& args]
      (with-command-line args
        "text2epub-clj -- Convert plain text to EPUB
-Usage: java -jar text2epub-standalone.jar [-df|-pt|-md] [-t \"epub title\"] <textfiles>.."
+Usage: java -jar text2epub-standalone.jar [-df|-pt|-md] [-t \"epub title\"] [-sb] <textfiles>.."
        [[default?  df?  "easy-markup text" true]
         [plain?    pt?  "plain text"]
         [markdown? md?  "markdown format text"]
         [title t        "EPUB title" "Untitled"]
+        [standby?  sb?  "run as standby-mode(output EPUB repeatedly when modify input files)" false]
         filenames]
        (let [marktype (cond
                        plain?    :plain
                        markdown? :markdown
-                       default?  :easy-markup
-                       :else     :easy-markup)]
-         (let [name   (.getName (File. (first filenames)))
-               output (str (replace-re #"\..+$" "" name) ".epub") ; output at current directory
-               info   {:title title :input filenames :markup marktype}
-               epub   (text->epub info)]
-           (epub->file epub output)
-           ;run report
-           (println "input:  " filenames)
-           (println "title:  " title)
-           (println "output: " output))))))
+                       default?  :easy-markup)
+             name   (.getName (File. (first filenames)))
+             output (str (replace-re #"\..+$" "" name) ".epub") ; output at current directory
+             info   {:title title :input filenames :markup marktype}]
+         (if (not standby?)
+           (convert output info)
+           (standby-loop output info))))))
